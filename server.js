ecc1ba0248a7ac9409a0e97c08e2428338f844bc
Nirmal Todwal <nirmaltodwal566@gmail.com>
2026-03-19 16:37:55 +0530

    fix render deploy: add base root route

--- server.js
+++ server.js
@@ -23,5 +23,9 @@ app.get('/api', (req, res) => {
   res.send('API is running...');
 });
 
+app.get("/", (req, res) => {
+  res.send("StockMaster Backend Running ✅");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
